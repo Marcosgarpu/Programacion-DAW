2caec9d6f63a72e0799320908e8d04d9d3b1d3df
Marcosgarpu <maarcosgarciapuche@gmail.com>
2026-05-26 17:19:16 +0200

    Delete Herencia directory

--- Herencia/src/Relacion_11/EJ10/Arrancable.java
+++
@@ -1,7 +0,0 @@
-package Relacion_11.EJ10;
-
-public interface Arrancable {
-    public abstract void detener();
-    public abstract void arrancar();
-    public abstract boolean estaArrancado();
-}
